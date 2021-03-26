2b164d561e667435e061a303b8e5b8949a213cc7
Sebastian Junges <sebastian.junges@gmail.com>
2021-03-25 21:58:35 -0700

    refactoring, create simplified circuit for automaton

--- aiger_jani/translation.py
+++ aiger_jani/translation.py
@@ -13,10 +13,11 @@ class JaniIntegerVariable:
     """
     Integer variables in JANI
     """
-    def __init__(self, name, lower_bound : int, upper_bound : int):
+    def __init__(self, name, lower_bound : int, upper_bound : int, is_local : bool):
         self.name = name
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
+        self.is_local = is_local
 
 
 class JaniScope:
@@ -25,7 +26,7 @@ class JaniScope:
         self._aigvars = {}
         self._local = False
 
-    def add_bounded_int_variable(self, name, lower_bound : int, upper_bound : int):
+    def add_bounded_int_variable(self, name, lower_bound : int, upper_bound : int) -> None:
         """
         Add bounded integer variable to the scope
         :param name: name of the variable
@@ -35,7 +36,7 @@ class JaniScope:
         """
         if name in self._variables:
             raise ValueError(f"Variable with name {name} already exists in scope.")
-        self._variables[name] = JaniIntegerVariable(name, lower_bound, upper_bound)
+        self._variables[name] = JaniIntegerVariable(name, lower_bound, upper_bound, self._local)
         self._aigvars[name]  = BV.atom(int(np.ceil(np.log(upper_bound - lower_bound))), name)
 
     def make_local_scope_copy(self) -> JaniScope:
@@ -47,8 +48,15 @@ class JaniScope:
         local_scope._local = True
         return local_scope
 
-    def get_aig_variable(self, name : str):
+    def get_aig_variable(self, name : str) -> BV.UnsignedBVExpr:
         return self._aigvars[name]
+
+    def is_local_var(self, name : str) -> bool:
+        return self._variables[name].is_local
+
+    @property
+    def variables(self):
+        return list(self._variables.values())
 
 
 class AutomatonContext:
@@ -75,7 +83,7 @@ class AutomatonContext:
             func = aiger_discrete.from_aigbv(
                 atom.aigbv, input_encodings={name: encoder}
             )
-            self._distributions[dist] = C.pcirc(func).randomize({name : { f"sel-{index}": prob for index, prob in enumerate(probs)}})
+            self._distributions[dist] = C.pcirc(func).randomize({name : { f"sel-{index}": prob for index, prob in enumerate(probs)}}).with_coins_id(f"{self._aut_name}_c{len(self._distributions)}")
         return self._distributions[dist]
 
     def register_action(self, action : str):
@@ -97,16 +105,20 @@ def _translate_constants(data : dict, scope : JaniScope):
 
 def _translate_variables(data : dict, scope : JaniScope):
     for v in data:
+        if "name" not in v:
+            raise ValueError("Variable not named")
+        name = v["name"]
         if v["type"]["kind"] != "bounded":
             raise ValueError("Only bounded variables are supported")
         if v["type"]["base"] != "int":
             raise ValueError("Only integer-typed variables are supported")
         #TODO could be a constant expression, which is not yet supported
         lower_bound = v["type"]["lower-bound"]
+        if lower_bound != 0:
+            raise NotImplementedError("Variable {name} has a non-zero lower bound. This is currently not supported")
         #TODO could be a constant expression, which is not yet supported
         upper_bound = v["type"]["upper-bound"]
         scope.add_bounded_int_variable(v["name"], lower_bound, upper_bound)
-
 
 
 _BINARY_AEX_OPERATORS = frozenset(["+", "-"])
@@ -115,7 +127,6 @@ _BINARY_AEX_OP_MAP = {"+" : lambda x, y: x + y,
 _BINARY_BOOL_OPERATORS = frozenset(["≤","≥"])
 _BINARY_BOOL_OP_MAP = {"≤" : lambda x, y: x <= y ,
                        "≥" : lambda x, y: x >= y}
-
 
 
 def _translate_expression(data : dict, scope : JaniScope):
@@ -152,79 +163,124 @@ def _parse_prob(data):
 def _selector(selector_bits, outputs, index = 0):
     if index == len(outputs) - 1:
         return outputs[index]
-    return BV.ite(selector_bits == BV.uatom(1,index), outputs[index], _selector(selector_bits, outputs, index + 1))
+    return BV.ite(selector_bits == BV.uatom(selector_bits.size, index), outputs[index], _selector(selector_bits, outputs, index + 1))
 
+def _translate_destinations(data : dict, ctx : AutomatonContext) -> set[str]:
+    """
 
-def _translate_edges(data : dict, ctx ):
-    max_internal_nondet_bits = int(np.ceil(np.log(len(data))))
-    max_action_bits = int(np.ceil(np.log(len(data) + 1))) # +1 due to reserving space for silent act
+    :param data: Describes the destinations.
+    :param ctx:
+    :return:
+    """
+    # Create coin flipping part.
+    if len(data) == 1:
+        pass
+    else:
+        probs = []
+        for d in data:
+            probs.append(_parse_prob(d["probability"]["exp"]))
+        prob_input = ctx.register_distribution(probs)
+        # TODO consider what to do with the additional output of prob_input
 
-    select_edge_expr = []
+    vars_written_to = set()
+    for d in data:
+        for a in d["assignments"]:
+            vars_written_to.add(a["ref"])
+    destinations = []
+    for index, d in enumerate(data):
+        assert d["location"] == "l"
+        updates = {}
+        # TODO add location handling
+        for assignment in d["assignments"]:
+            var_primed = assignment["ref"]
+            if len(data) > 1:
+                var_primed += "-" + str(index)
+            updates[var_primed] = _translate_expression(assignment["value"], ctx.scope).with_output(var_primed)
+        for var in vars_written_to:
+            var_primed = var
+            if len(data) > 1:
+                var_primed += "-" + str(index)
+            if var_primed not in updates:
+                updates[var_primed] = ctx.scope.get_aig_variable(var).with_output(var_primed)
+
+        # CONCAT:
+        updates = list(updates.values())
+        update = updates[0].aigbv
+        for up in updates[1:]:
+            update = update | up.aigbv
+        destinations.append(update)
+
+    edge_circuit = destinations[0]
+    for d in destinations[1:]:
+        edge_circuit = edge_circuit | d
+
+    vars_written_to = list(vars_written_to)
+    if len(destinations) > 1:
+        selector = _selector(BV.uatom(int(np.ceil(np.log(len(probs)))), 'sel'),
+                             [BV.uatom(ctx.scope.get_aig_variable(vars_written_to[0]).size,
+                                       vars_written_to[0] + "-" + str(index)) for index in
+                              range(len(destinations))]).with_output(vars_written_to[0]).aigbv
+        for var in vars_written_to[1:]:
+            selector = selector | _selector(BV.uatom(int(np.ceil(np.log(len(probs)))), 'sel'),
+                                            [BV.uatom(ctx.scope.get_aig_variable(var).size, var + "-" + str(index)) for
+                                             index in range(len(destinations))]).with_output(var).aigbv
+
+        edge_circuit = edge_circuit >> selector
+        edge_circuit = prob_input >> edge_circuit
+    return edge_circuit, vars_written_to
+
+def _translate_edges(data : dict, ctx : AutomatonContext ):
+    #TODO handle notion of actions
+    # max_internal_nondet_bits = int(np.ceil(np.log(len(data))))
+    # max_action_bits = int(np.ceil(np.log(len(data) + 1))) # +1 due to reserving space for silent act
+    #
+    # select_edge_expr = []
     edge_circuits =  []
-    for edge in data:
+    for edge_index, edge in enumerate(data):
         #TODO add location handling
         assert edge["location"] == "l"
-        act, ndet = ctx.register_action(edge["action"])
-        select_edge_expr.append(BV.uatom(max_action_bits, 'act') == BV.uatom(max_action_bits, act) & \
-                           BV.uatom(max_internal_nondet_bits, 'nd') == BV.uatom(max_internal_nondet_bits, ndet)
+        #TOD
+        #act, ndet = ctx.register_action(edge["action"])
+        #select_edge_expr.append((BV.uatom(max_action_bits, 'act') == BV.uatom(max_action_bits, act)) & \
+        #                        (BV.uatom(max_internal_nondet_bits, 'nd') == BV.uatom(max_internal_nondet_bits, ndet)))
 
+
+        edge_circuit, vars_written_to = _translate_destinations(edge["destinations"], ctx)
         if "guard" in edge:
-            guard_expr = _translate_expression(edge["guard"]["exp"], ctx.scope)
-        else:
-            #TODO add true
-            pass
-        if len(edge["destinations"]) == 1:
-            pass
-        else:
-            probs = []
-            for d in edge["destinations"]:
-                probs.append(_parse_prob(d["probability"]["exp"]))
-            prob_input = ctx.register_distribution(probs)
-            #TODO consider what to do with the additional output of prob_input
+            guard_expr = _translate_expression(edge["guard"]["exp"], ctx.scope).with_output("enabled")
+            edge_circuit = edge_circuit | guard_expr.aigbv
 
-        vars_written_to = set()
-        for d in edge["destinations"]:
-            for a in d["assignments"]:
-                vars_written_to.add(a["ref"])
-        destinations = []
-        for index, d in enumerate(edge["destinations"]):
-            assert d["location"] == "l"
-            updates = {}
-            #TODO add location handling
-            for assignment in d["assignments"]:
-                var_primed = assignment["ref"] + "'" + "-" + str(index)
-                updates[var_primed] = _translate_expression(assignment["value"], ctx.scope).with_output(var_primed)
-            for var in vars_written_to:
-                var_primed = var + "'" + "-" + str(index)
-                if var_primed not in updates:
-                    updates[var_primed] = ctx.scope.get_aig_variable(var).with_output(var_primed)
+        # Make sure that the edge circuit treats all variables as outputs
+        # Additionally, mark whether global variables have been written to.
+        for v in ctx.scope.variables:
+            if not v.is_local:
+                if v.name in vars_written_to:
+                    edge_circuit = edge_circuit | (BV.source(wordlen=1, value=1, name=v.name + "-mod", signed=False))
+                else:
+                    edge_circuit = edge_circuit | (BV.source(wordlen=1, value=0, name=v.name + "-mod", signed=False))
+            if v.name not in vars_written_to:
+                edge_circuit = edge_circuit | ctx.scope.get_aig_variable(v.name).with_output(v.name).aigbv
 
-            # CONCAT:
-            updates = list(updates.values())
-            update = updates[0].aigbv
-            for up in updates[1:]:
-                update = update | up.aigbv
-            destinations.append(update)
-
-        edge_circuit = destinations[0]
-        for d in destinations[1:]:
-            edge_circuit = edge_circuit | d
-
-        vars_written_to = list(vars_written_to)
-
-        if len(destinations) > 1:
-            #TODO replace 2
-            print("Construct selector")
-            selector = _selector(BV.uatom(int(np.ceil(np.log(len(probs)))),'sel'),
-                                   [BV.uatom(2, vars_written_to[0] + "'" + "-" + str(index)) for index in range(len(destinations))]).with_output(vars_written_to[0]).aigbv
-            for var in vars_written_to[1:]:
-                #TODO replace 2
-                selector = selector | _selector(BV.uatom(int(np.ceil(np.log(len(probs)))),'sel'),
-                                   [BV.uatom(2, var + "'" + "-" + str(index)) for index in range(len(destinations))]).with_output(var).aigbv
-            edge_circuit = edge_circuit >> selector
-            edge_circuit = prob_input >> edge_circuit
-
+        # Rename outputs such that we can later merge them.
+        edge_circuit = edge_circuit['o', { o : o + "-" + str(edge_index) for o in edge_circuit.outputs}]
         edge_circuits.append(edge_circuit)
+
+
+    print("##########################################")
+    for c in edge_circuits:
+        print(c)
+    aut_circuit = edge_circuits[0]
+    for c in edge_circuits[1:]:
+        aut_circuit = aut_circuit | c
+
+    selector = _selector(BV.uatom(int(np.ceil(np.log(len(edge_circuits)))),'edge'),
+                                   [BV.uatom(ctx.scope.get_aig_variable(ctx.scope.variables[0].name).size, ctx.scope.variables[0].name + "-" + str(index)) for index in range(len(edge_circuits))]).with_output(ctx.scope.variables[0].name).aigbv
+    for v in ctx.scope.variables[1:]:
+        selector = selector | _selector(BV.uatom(int(np.ceil(np.log(len(edge_circuits)))), 'edge'),
+                             [BV.uatom(ctx.scope.get_aig_variable(v.name).size, v.name + "-" + str(index)) for index in
+                              range(len(edge_circuits))]).with_output(v.name).aigbv
+    aut_circuit = aut_circuit >> selector
+    print(aut_circuit)
 
 
 def _create_automaton_context(data : dict, scope : JaniScope):
@@ -245,12 +301,13 @@ def _translate_automaton(data : dict, scope : JaniScope):
     ctx = _create_automaton_context(data, scope)
     _translate_variables(data["variables"], scope)
     _translate_edges(data["edges"], ctx)
-
+    #TODO return something
 
 def translate_file(path):
     with open(path) as f:
         jani_enc = json.load(f)
-    translate_jani(jani_enc)
+    return translate_jani(jani_enc)
+
 
 
 def translate_jani(data : json):
@@ -259,7 +316,7 @@ def translate_jani(data : json):
     _translate_variables(data["variables"], global_scope)
     for aut in data["automata"]:
         _translate_automaton(aut, global_scope.make_local_scope_copy())
-
+    #TODO do something with the automata.
 
 
 
