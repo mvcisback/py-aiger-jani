a6afdd1bfc70f13e7bc345a184ed1e34069beeb6
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-05-06 19:40:57 -0700

    Added tests.

--- tests/test_aiger_jani.py
+++ tests/test_aiger_jani.py
@@ -12,14 +12,38 @@ def test_smoke():
     pass
 
 
+def estimate_prob(query, N=10_000):
+    assert len(query.outputs) == 1
+    output, *_ = query.outputs
+    count = 0
+    for _ in range(N):
+        try:
+            count += query({})[0][output]
+        except:
+            N -= 1
+        
+    return count / N
+
+
 def test_minimdp():
     circ = aiger_jani.translation.translate_file("tests/minimdp.jani")
     assert circ.outputs == {'main-x', 'main-y'}
 
-    circ <<= BV.source(1, 0, 'edge', False)
-    circ >>= BV.sink(2, ['main-y'])
-    circ >>= (BV.uatom(2, 'main-x') == 3).aigbv
+    # Fix edge and check probability of ending on x=3 given valid run.
+    query = circ
+    query <<= BV.source(1, 0, 'edge', False)
+    query >>= BV.sink(2, ['main-y'])
+    query >>= (BV.uatom(2, 'main-x') == 3).aigbv
 
-    assert infer.prob(circ.unroll(1)) == approx(0)
-    assert infer.prob(circ.unroll(2, only_last_outputs=True)) == approx(1/4)
-    assert infer.prob(circ.unroll(3, only_last_outputs=True)) == approx(1/3)
+    assert infer.prob(query.unroll(1, only_last_outputs=True)) == approx(0)
+    assert infer.prob(query.unroll(2, only_last_outputs=True)) == approx(1/4)
+    assert infer.prob(query.unroll(3, only_last_outputs=True)) == approx(1/3)
+
+    # Randomize edge and check probability of ending on x=y given valid run.
+    query = circ.randomize({'edge': {0: 0.5, 1: 0.5}})
+
+    query >>= (BV.uatom(2, 'main-x') == BV.uatom(2, 'main-y')).aigbv
+
+    assert infer.prob(query.unroll(1, only_last_outputs=True)) == approx(1/4, abs=1e-2)
+    assert infer.prob(query.unroll(2, only_last_outputs=True)) == approx(1/4, abs=1e-2)
+    assert infer.prob(query.unroll(3, only_last_outputs=True)) == approx(13/38)
