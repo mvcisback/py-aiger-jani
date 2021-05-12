ddf24d0e2c5aadd87e8c73401f5ef7990e828240
Sebastian Junges <sebastian.junges@gmail.com>
2021-05-12 16:40:27 -0700

    Revert "Refactor to simplify logic."
    This reverts commit 8d75329db4c55e63ce2c23b0d7d1af22ecc23ca0.

--- aiger_jani/translation.py
+++ aiger_jani/translation.py
@@ -294,16 +294,30 @@ def _translate_destinations(data: dict, ctx: AutomatonContext) -> set[str]:
     :param ctx:
     :return:
     """
+    # Create coin flipping part.
+    if len(data) == 1:
+        pass
+    else:
+        probs = []
+        for d in data:
+            probs.append(_parse_prob(d["probability"]["exp"]))
+        prob_input = ctx.register_distribution(probs)
+        # TODO consider what to do with the additional output of prob_input
 
-    vars_written_to = {a["ref"] for d in data for a in d["assignments"]}
+    vars_written_to = set()
+    for d in data:
+        for a in d["assignments"]:
+            vars_written_to.add(a["ref"])
 
     destinations = []
     for index, d in enumerate(data):
-        assert d["location"] == "l"  # TODO add location handling
-
+        assert d["location"] == "l"
         updates = {}
+        # TODO add location handling
         for assignment in d["assignments"]:
-            var_primed = f"{assignment['ref']}-{index}"
+            var_primed = assignment["ref"]
+            if len(data) > 1:  # TODO: why make this case special?
+                var_primed += f"-{index}"
 
             val = assignment["value"]
             updates[var_primed] = _translate_expression(val, ctx.scope) \
@@ -311,19 +325,21 @@ def _translate_destinations(data: dict, ctx: AutomatonContext) -> set[str]:
                 .aigbv
 
         for var in vars_written_to:
-            var_primed = f"{var}-{index}"
+            var_primed = var
+            if len(data) > 1:
+                var_primed += f"-{index}"  # TODO: why make this case special.
             if var_primed not in updates:
-                updates[var_primed] = ctx.scope.get_aig_variable(var) \
-                                               .with_output(var_primed) \
-                                               .aigbv
+                updates[var_primed] = ctx.scope \
+                                         .get_aig_variable(var) \
+                                         .with_output(var_primed) \
+                                         .aigbv
 
         update = par_compose(updates.values())
         destinations.append(update)
 
     edge_circuit = par_compose(destinations)
-    edge_circuit = C.pcirc(edge_circuit)  # Force probabilistic circuit.
 
-    vars_written_to = list(vars_written_to)  # Fix ordering.
+    vars_written_to = list(vars_written_to)
     if len(destinations) > 1:
         indices = range(len(destinations))
 
@@ -333,16 +349,11 @@ def _translate_destinations(data: dict, ctx: AutomatonContext) -> set[str]:
                 outputs = [BV.uatom(size, f"{var}-{idx}") for idx in indices]
                 yield mux(outputs, key_name='sel').with_output(var).aigbv
 
-        # Create coin flipping part.
-        probs = [_parse_prob(d["probability"]["exp"]) for d in data]
-        prob_input = ctx.register_distribution(probs)
-        # TODO consider what to do with the additional output of
-        # prob_input.
-
         edge_circuit >>= par_compose(selectors())
         edge_circuit <<= prob_input
+    else:
+        edge_circuit = C.pcirc(edge_circuit)  # Deterministic pcirc.
 
-    edge_circuit = C.pcirc(edge_circuit)  # Force probabilistic circuit.
     return edge_circuit, vars_written_to
 
 
